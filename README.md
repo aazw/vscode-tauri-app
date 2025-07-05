7f56ff01303ef8ca28235bce00d4244a5b527665
aazw <3594744+aazw@users.noreply.github.com>
2025-07-05 10:48:40 +0900

    tauriのscaffoldを追加

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# vscode-base
+# Tauri + React + Typescript
 
-This project is a template implementation for projects that use a VS Code Dev Container.
+This template should help get you started developing with Tauri, React and Typescript in Vite.
+
+## Recommended IDE Setup
+
+- [VS Code](https://code.visualstudio.com/) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer)
---
+++ scripts/build_for_mac.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -eu
+
+# How do I get the directory where a Bash script is located from within the script itself?
+# https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+cd ${SCRIPT_DIR}/..
+
+# macOSかどうかを確認
+if [[ "$(uname)" != "Darwin" ]]; then
+	echo "Error: This script is only for macOS"
+	exit 1
+fi
+
+# 依存関係インストール
+npm install
+
+# macOS向けにビルド
+npm run tauri build -- --target aarch64-apple-darwin
